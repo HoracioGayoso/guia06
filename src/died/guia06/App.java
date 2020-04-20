a158b94c9f8066fe6e34b9dace41156f91d1d8f7
Horacio <horaciogayoso9@gmail.com>
2020-04-20 12:48:31 -0300

    resuelvo paso 09

--- src/died/guia06/App.java
+++ src/died/guia06/App.java
@@ -13,7 +13,85 @@ public class App {
 		Curso C2 = new Curso("Algebra", 2020, 5, 10, 0);
 		Curso C3 = new Curso("Fisica", 2020, 3, 100, 0);
 		Curso C4 = new Curso("AEDD", 2020, 3, 100, 0);
+		Curso C5 = new Curso("Ingles", 2020, 5, 10, 0);
 		
+		System.out.println("SE TRABAJA CON METODO INSCRIBIR CON EXCEPCIONES");
+		System.out.println("EL TRABAJO CON EL OTRO METODO QUEDA COMENTADO DEBAJO");
+
+		System.out.println("Se desea inscribir al alumno: "+a1.getNombre()+" al curso: "+C1.getNombre());
+		try {
+			C1.inscribirAlumno(a1);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("--------------------------");
+		
+		try {
+		C5.inscribirAlumno(a1);
+		a1.aprobar(C5);
+		C2.inscribirAlumno(a1);
+		C3.inscribirAlumno(a1);
+		C4.inscribirAlumno(a1);
+		System.out.println("El alumno: "+a1.getNombre()+" cursa las siguientes materias: ");
+		System.out.println(a1.getCursando());
+		}catch (Exception e) {}
+		System.out.println("Se desea inscribir al alumno: "+a1.getNombre()+" al curso: "+C1.getNombre());
+		try {
+			C1.inscribirAlumno(a1);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("--------------------------");
+		
+		try {
+		C3.inscribirAlumno(a2);
+		C3.inscribirAlumno(a3);
+		System.out.println("Alumnos inscriptos al curso: "+C3.getNombre());
+		C3.imprimirInscriptos(Orden.ALFABETICAMENTE);
+		}catch(Exception e) {}
+		System.out.println("Se desea inscribir al alumno: "+a4.getNombre()+" al curso: "+C3.getNombre());
+		try {
+			C3.inscribirAlumno(a4);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println("--------------------------");
+		try {
+			System.out.println("Se desea inscribir al alumno: "+ a3.getNombre()+ " al curso "+ C5.getNombre());
+			C5.inscribir(a3);
+			System.out.println("Inscripcion exitosa");
+		}catch (Exception e) {System.out.println(e.getMessage());}
+		
+		System.out.println("--------------------------");
+		
+		try {
+		C2.inscribirAlumno(a4);
+		C4.inscribirAlumno(a4);
+		C5.inscribirAlumno(a4);
+		a4.aprobar(C5);
+		System.out.println("Materias que el alumno "+a4.getNombre()+ " esta cursando: ");
+		System.out.println(a4.getCursando());
+		System.out.println("Creditos del alumno "+a4.getNombre()+ ": "+ a4.creditosObtenidos());
+	}catch (Exception e) {} 
+	
+		System.out.println("--------------------------");
+		
+		try {
+			C2.inscribirAlumno(a2);
+			C2.inscribirAlumno(a3);
+			C2.inscribirAlumno(a5);
+			System.out.println("Alumnos inscriptos a "+C2.getNombre()+" ordenados Alfabeticamente:");
+			C2.imprimirInscriptos(Orden.ALFABETICAMENTE);
+			System.out.println("Alumnos inscriptos a "+C2.getNombre()+" ordenados por NROLIBRETA:");
+			C2.imprimirInscriptos(Orden.NROLIBRETA);
+			System.out.println("Alumnos inscriptos a "+C2.getNombre()+" ordenados por cantidad de creditos:");
+			C2.imprimirInscriptos(Orden.CREDITOS);
+		}catch (Exception e) {}
+		
+		/*
 		C2.inscribir(a1);
 		C2.inscribir(a2);
 		C2.inscribir(a3);
@@ -60,5 +138,7 @@ public class App {
 		
 		System.out.println("Cantidad de creditos del alumno "+ a3.getNombre()+": "+a3.creditosObtenidos());
 		System.out.println("Cantidad de materias cursando del alumno "+ a3.getNombre()+": "+a3.materiasCursando());
-	}
+	*/
+		
+	}	
 }
--- src/died/guia06/util/TodasMateriasRegularesExcepcion.java
+++ src/died/guia06/util/TodasMateriasRegularesExcepcion.java
@@ -2,7 +2,7 @@ package died.guia06.util;
 
 public class TodasMateriasRegularesExcepcion extends Exception {
 	public TodasMateriasRegularesExcepcion(){
-		super("El Alumno esta regular en todas las materias del corriente ciclo lectivo");	
+		super("El Alumno cursa demasiadas materias este ciclo lectivo");	
 	}
 	
 }
